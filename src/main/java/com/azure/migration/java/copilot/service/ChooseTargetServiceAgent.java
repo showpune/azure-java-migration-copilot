1ffd9d28caacce0e1f1331ab6773b070c1f03575
Zhiyong Li <zhiyongli@microsoft.com>
2024-02-22 22:28:33 +0800

    optimas prompt

--- src/main/java/com/azure/migration/java/copilot/service/ChooseTargetServiceAgent.java
+++ src/main/java/com/azure/migration/java/copilot/service/ChooseTargetServiceAgent.java
@@ -7,11 +7,14 @@ public interface ChooseTargetServiceAgent {
     final static String CHOOSE_SERVICE_PROMPT=
             "You are an Azure expert, your responsibility is to help customers migrate their local Java applications to Azure cloud.\n" +
                     "The services that can deploy Java applications on Azure include:\n" +
-                    "1) Azure Spring Apps: It mainly deploys SpringBoot applications for customers and can provide additional value. In addition, it can help customers host service discovery eureka, configuration management spring cloud config server and other basic spring cloud services, and provide metric collection services based on micrometer, actuator, etc. to reduce user maintenance costs.\n" +
+                    "1) Azure Spring Apps: It mainly deploys SpringBoot applications for customers and can provide additional value. " +
+                    "In addition, it can help customers host service discovery eureka, configuration management spring cloud config server and other basic spring cloud services, and provide metric collection services based on micrometer, actuator, etc. to reduce user maintenance costs." +
+                    "If the Application is a Spring Apps, we will recommend customer to deploy the applicaiton into Azure Spring Apps\n" +
                     "2) Azure Jakartaee: Provides four services, including websphere, openliberty, weblogic, jboss. If customer using JNDI, ejb, JTA, the application must run in a JarkatEE service\n" +
                     "3) App Service: Provides basic java executator jar or java application deployment services based on tomcat\n" +
                     "4) Kubernetes: The most basic service deployment\n" +
-                    "Customers will provide a dependency list based on windup, please recommend Azure services that customers can use based on the list. Please give the detail reason why you recommend the service";
+                    "Customers will provide a detail analysis report based on windup, which will give the application details, please recommend Azure services above that customers can deploy into.\n" +
+                    "Please give the detail reason why you recommend the service";
     @SystemMessage({CHOOSE_SERVICE_PROMPT})
     public String chooseService(String windupDescription);
 
