8c2621e4e34f95ba9dd66f1004c3c0b2e0a8c0a5
Kun Sun <kunsun@microsoft.com>
2024-07-08 16:29:03 +0800

    Adding the logic of copying bicep files to source directory

--- azure-java-migration-copilot-client/src/main/java/com/azure/migration/java/copilot/service/generate/AzdConfigFilesGenerator.java
+++ azure-java-migration-copilot-client/src/main/java/com/azure/migration/java/copilot/service/generate/AzdConfigFilesGenerator.java
@@ -1,71 +1,70 @@
-package com.azure.migration.java.copilot.service.util;
+package com.azure.migration.java.copilot.service.generate;
 
 import com.azure.migration.java.copilot.service.MigrationContext;
 import com.azure.migration.java.copilot.service.constant.Constants;
 import com.azure.migration.java.copilot.service.model.bicep.*;
 import com.azure.migration.java.copilot.service.model.template.DbTemplateContext;
 import com.azure.migration.java.copilot.service.model.template.TemplateContext;
+import com.azure.migration.java.copilot.service.util.FileUtil;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
-
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 
 @Component
 public class AzdConfigFilesGenerator {
 
-    @Autowired
-    private MigrationContext migrationContext;
+    @Value("${copilot.bicpe.tempalte.path}")
+    String bicepTemplatePath;
 
-    public void genereateBicepParamsFiles() {
-        try {
-            Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
-            String jsonStr = gson.toJson(this.assembleBicepParmas());
-            String filePath = migrationContext.getSourceCodePath() + "\\infra\\main.parameters.json";
-            Files.write(Paths.get(filePath), jsonStr.getBytes());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    public void generateBicepFiles(MigrationContext migrationContext) throws Exception {
+        generateBicepParamsFiles(migrationContext);
+        copyBicepFiles(migrationContext.getSourceCodePath());
     }
 
-    public BicepParams assembleBicepParmas() {
+    public void generateBicepParamsFiles(MigrationContext migrationContext) throws IOException {
+            Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+            String jsonStr = gson.toJson(this.assembleBicepParmas(migrationContext.getTemplateContext()));
+            Path filePath = Paths.get(migrationContext.getSourceCodePath(),"/infra", "/main.parameters.json");
+            Files.write(filePath, jsonStr.getBytes());
+
+    }
+
+    public BicepParams assembleBicepParmas(TemplateContext templateContext) {
         BicepParams bicepParams = new BicepParams();
         bicepParams.setSchema("https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#");
         bicepParams.setContentVersion("1.0.0.0");
-        String dbName = "migrationdemosql";
+
         Parameters parameters = new Parameters();
         bicepParams.setParameters(parameters);
         parameters.getEnvironmentName().setValue("${AZURE_ENV_NAME}");
         parameters.getLocation().setValue("${AZURE_LOCATION}");
         parameters.getSpringPetclinicExists().setValue("${SERVICE_SPRING_PETCLINIC_RESOURCE_EXISTS=false}");
         parameters.getPrincipalId().setValue("${AZURE_PRINCIPAL_ID}");
-        parameters.getDbName().setValue(dbName);
 
         String defaultCommentName = "The name of the environment variable when running in Azure. If empty, ignored.";
         String defaultCommentValue = "The value to provide. This can be a fixed literal, or an expression like ${VAR} "
                 + "to use the value of 'VAR' from the current environment.";
-        TemplateContext templdateContext = migrationContext.getTemplateContext();
-        DbTemplateContext dbTemplateContext = templdateContext.getDbTemplateContext();
-        dbTemplateContext.setType("mysql");
-        dbTemplateContext.setName(dbName);
-        dbTemplateContext.setPort(3306);
-        dbTemplateContext.setSchema("petclinic");
-        dbTemplateContext.setUser("migrationtool");
-        dbTemplateContext.setPwd("Password@123");
+        DbTemplateContext dbTemplateContext = templateContext.getDbTemplateContext();
+        String dbName = dbTemplateContext.getName();
+        parameters.getDbName().setValue(dbName);
         String dbUrl = Constants.JDBC + Constants.COLON
             + dbTemplateContext.getType() + Constants.COLON + Constants.DOUBLE_SLASH
-            + dbTemplateContext.getName() + Constants.Azure_MYSQL_DOMAIN_SUFFIX + Constants.COLON
+            + dbName + Constants.Azure_MYSQL_DOMAIN_SUFFIX + Constants.COLON
             + dbTemplateContext.getPort() + Constants.SLASH
             + dbTemplateContext.getSchema() + Constants.Azure_MYSQL_CONN_STRING_SUFFIX;
         List<SettingItem> settingItems = new ArrayList<>() {{
@@ -81,10 +80,10 @@ public class AzdConfigFilesGenerator {
         return bicepParams;
     }
 
-    public void copyBicepFiles() {
+    public void copyBicepFiles(String targetPath) throws IOException {
         // Define the source and destination directories
-        String sourcePath = "D:\\code\\sourceCode\\migrationDemo\\azure-java-migration-copilot\\azure-java-migration-copilot-client\\src\\main\\resources\\azd-template-fils";
-        String targetPath = migrationContext.getSourceCodePath();
-        FileUtil.copyFiles(sourcePath, targetPath);
+        Resource resource = resourceLoader.getResource(bicepTemplatePath);
+        File file = resource.getFile();
+        FileUtil.copyFiles(file.getAbsolutePath(), targetPath);
     }
 }
--- azure-java-migration-copilot-client/src/main/java/com/azure/migration/java/copilot/service/util/FileUtil.java
+++ azure-java-migration-copilot-client/src/main/java/com/azure/migration/java/copilot/service/util/FileUtil.java
@@ -6,30 +6,24 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileUtil {
 
-    public static void copyFiles(String sourcePath, String targetPath) {
-        try {
-            Path sourceDirectory = Path.of(sourcePath);
-            Path targetDirectory = Path.of(targetPath);
+    public static void copyFiles(String sourcePath, String targetPath) throws IOException {
+        Path sourceDirectory = Path.of(sourcePath);
+        Path targetDirectory = Path.of(targetPath);
 
-            Files.walkFileTree(sourceDirectory, new SimpleFileVisitor<>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    Path targetFile = targetDirectory.resolve(sourceDirectory.relativize(file));
-                    Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
-                    return FileVisitResult.CONTINUE;
-                }
+        Files.walkFileTree(sourceDirectory, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path targetFile = targetDirectory.resolve(sourceDirectory.relativize(file));
+                Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                return FileVisitResult.CONTINUE;
+            }
 
-                @Override
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                    Path targetDir = targetDirectory.resolve(sourceDirectory.relativize(dir));
-                    Files.createDirectories(targetDir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-            System.out.println("Files copied successfully!");
-        } catch (IOException e) {
-            System.out.println("Failed to traverse the directory");
-            e.printStackTrace();
-        }
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Path targetDir = targetDirectory.resolve(sourceDirectory.relativize(dir));
+                Files.createDirectories(targetDir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 }
