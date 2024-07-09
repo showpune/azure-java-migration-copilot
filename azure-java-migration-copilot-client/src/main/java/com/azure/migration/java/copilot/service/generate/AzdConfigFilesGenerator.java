0e45f663834779ad54f44471b0a9864e1083cd75
Kun Sun <kunsun@microsoft.com>
2024-07-09 12:45:50 +0800

    Adding env generating logic

--- azure-java-migration-copilot-client/src/main/java/com/azure/migration/java/copilot/service/generate/AzdConfigFilesGenerator.java
+++ azure-java-migration-copilot-client/src/main/java/com/azure/migration/java/copilot/service/generate/AzdConfigFilesGenerator.java
@@ -4,10 +4,12 @@ import com.azure.migration.java.copilot.service.MigrationContext;
 import com.azure.migration.java.copilot.service.constant.Constants;
 import com.azure.migration.java.copilot.service.model.bicep.*;
 import com.azure.migration.java.copilot.service.model.template.DbTemplateContext;
+import com.azure.migration.java.copilot.service.model.template.EnvVariableTemplateContext;
 import com.azure.migration.java.copilot.service.model.template.TemplateContext;
 import com.azure.migration.java.copilot.service.util.FileUtil;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -30,6 +32,11 @@ public class AzdConfigFilesGenerator {
 
     @Autowired
     ResourceLoader resourceLoader;
+
+    String defaultCommentName = "The name of the environment variable when running in Azure. If empty, ignored.";
+
+    String defaultCommentValue = "The value to provide. This can be a fixed literal, or an expression like ${VAR} "
+        + "to use the value of 'VAR' from the current environment.";;
 
     public void generateBicepFiles(MigrationContext migrationContext) throws Exception {
         copyBicepFiles(migrationContext.getSourceCodePath());
@@ -55,29 +62,37 @@ public class AzdConfigFilesGenerator {
         parameters.getLocation().setValue("${AZURE_LOCATION}");
         parameters.getSpringPetclinicExists().setValue("${SERVICE_SPRING_PETCLINIC_RESOURCE_EXISTS=false}");
         parameters.getPrincipalId().setValue("${AZURE_PRINCIPAL_ID}");
-
-        String defaultCommentName = "The name of the environment variable when running in Azure. If empty, ignored.";
-        String defaultCommentValue = "The value to provide. This can be a fixed literal, or an expression like ${VAR} "
-                + "to use the value of 'VAR' from the current environment.";
         DbTemplateContext dbTemplateContext = templateContext.getDbTemplateContext();
-        String dbName = dbTemplateContext.getName();
-        parameters.getDbName().setValue(dbName);
-        String dbUrl = Constants.JDBC + Constants.COLON
-            + dbTemplateContext.getType() + Constants.COLON + Constants.DOUBLE_SLASH
-            + dbName + Constants.Azure_MYSQL_DOMAIN_SUFFIX + Constants.COLON
-            + dbTemplateContext.getPort() + Constants.SLASH
-            + dbTemplateContext.getSchema() + Constants.Azure_MYSQL_CONN_STRING_SUFFIX;
-        List<SettingItem> settingItems = new ArrayList<>() {{
-            add(new SettingItem(Constants.SPRING_DATASOURCE_URL, dbUrl, null, defaultCommentName, defaultCommentValue));
-            add(new SettingItem(Constants.SPRING_DATASOURCE_USERNAME, dbTemplateContext.getUser(), true, defaultCommentName, defaultCommentValue));
-            add(new SettingItem(Constants.SPRING_DATASOURCE_PASSWORD, dbTemplateContext.getPwd(), true, defaultCommentName, defaultCommentValue));
-        }};
+        parameters.getDbName().setValue(dbTemplateContext.getName());
+
+        List<SettingItem> settingItems = new ArrayList<>();
+        assembleDbEnvParams(settingItems, dbTemplateContext);
+        assembleEnvParams(settingItems, templateContext.getEnvironments());
         CommonItem<Settings> springPetclinicDefinition = new CommonItem<>();
         springPetclinicDefinition.setValue(new Settings());
         springPetclinicDefinition.getValue().setSettings(settingItems);
         parameters.setSpringPetclinicDefinition(springPetclinicDefinition);
 
         return bicepParams;
+    }
+
+    private void assembleEnvParams(List<SettingItem> settingItems, List<EnvVariableTemplateContext> envsList) {
+        for(EnvVariableTemplateContext item : envsList) {
+            settingItems.add(new SettingItem(item.getKey(), item.getValue(), false, defaultCommentName, defaultCommentValue));
+        }
+    }
+
+    @NotNull
+    private List<SettingItem> assembleDbEnvParams(List<SettingItem> settingItems, DbTemplateContext dbTemplateContext) {
+        String dbUrl = Constants.JDBC + Constants.COLON
+            + dbTemplateContext.getType() + Constants.COLON + Constants.DOUBLE_SLASH
+            + dbTemplateContext.getName() + Constants.Azure_MYSQL_DOMAIN_SUFFIX + Constants.COLON
+            + dbTemplateContext.getPort() + Constants.SLASH
+            + dbTemplateContext.getSchema() + Constants.Azure_MYSQL_CONN_STRING_SUFFIX;
+        settingItems.add(new SettingItem(Constants.SPRING_DATASOURCE_URL, dbUrl, null, defaultCommentName, defaultCommentValue));
+        settingItems.add(new SettingItem(Constants.SPRING_DATASOURCE_USERNAME, dbTemplateContext.getUser(), true, defaultCommentName, defaultCommentValue));
+        settingItems.add(new SettingItem(Constants.SPRING_DATASOURCE_PASSWORD, dbTemplateContext.getPwd(), true, defaultCommentName, defaultCommentValue));
+        return settingItems;
     }
 
     public void copyBicepFiles(String targetPath) throws IOException {
