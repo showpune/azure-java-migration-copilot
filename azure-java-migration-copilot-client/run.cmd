6c5caee1debcddf19b2c57c467689ae6ba1063d8
showpune <iamlizhiyong@outlook.com>
2024-08-08 15:03:32 +0800

    Build image for ford

--- azure-java-migration-copilot-client/run.cmd
+++ azure-java-migration-copilot-client/run.cmd
@@ -1,4 +1,5 @@
 set CODE_DIR=C:\IdeaProjects\rabbitmq-servicebus
 set FORCE=
-set RUNTIME=podman
+set RUNTIME=docker
+%RUNTIME% pull showpune/azure-migration-copilot:latest
 %RUNTIME% run -v "%TMP%:/tmp" -v "%CODE_DIR%:/code" --env-file env.list showpune/azure-migration-copilot:latest  "--codemigration=Migrate MQ to Service Bus" "%FORCE%"
--- azure-java-migration-copilot-client/run.sh
+++ azure-java-migration-copilot-client/run.sh
@@ -1,4 +1,5 @@
 export CODE_DIR=/mnt/c/IdeaProjects/rabbitmq-servicebus
 export FORCE=
 export RUNTIME=podman
+%RUNTIME% pull showpune/azure-migration-copilot:latest
 $RUNTIME run -v "/tmp:/tmp" -v "$CODE_DIR:/code" --env-file env.list showpune/azure-migration-copilot:latest  "--codemigration=Migrate MQ to Service Bus" "$FORCE"
