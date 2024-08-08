4659d2eea0ad342bbe4be5f48a865dd076355d50
showpune <iamlizhiyong@outlook.com>
2024-08-08 16:30:07 +0800

    Auto upgrade

--- azure-java-migration-copilot-client/build.sh
+++ azure-java-migration-copilot-client/build.sh
@@ -1,3 +1,7 @@
-mvn clean package -DskipTests=true
-docker build -t showpune/azure-migration-copilot:latest . -f Dockerfile
+mvn clean package -DskipTests=true --no-transfer-progress
+export tag=$(date +"%Y%m%d%H%M%S")
+docker build -t showpune/azure-migration-copilot:$tag . -f Dockerfile
+docker push showpune/azure-migration-copilot:$tag
+
+docker tag showpune/azure-migration-copilot:$tag showpune/azure-migration-copilot:latest
 docker push showpune/azure-migration-copilot:latest
